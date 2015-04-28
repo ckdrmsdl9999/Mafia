022f9defe0a21b5613a04db28e0390ff9c77dd30
Cory Gehr <gehrc621@gmail.com>
2015-04-28 18:48:21 -0400

    Add VoteSequence

--- Mafia/src/mafiaserver/VoteSequence.java
+++ Mafia/src/mafiaserver/VoteSequence.java
@@ -1,5 +1,7 @@
 package mafiaserver;
 
+import java.util.HashMap;
+
 /**
  * VoteSequence.java
  * Contains the VoteSequence class
@@ -7,4 +9,29 @@ package mafiaserver;
  */
 public class VoteSequence extends Thread {
     
+    private final HashMap<String, String> votes; // List of votes
+    
+    /**
+     * VoteSequence()
+     * Constructor for the VoteSequence class
+     */
+    public VoteSequence() {
+        this.votes = new HashMap();
+    }
+    
+    /**
+     * addVote()
+     * Adds a vote for a player
+     * @param client Player Voting
+     * @param vote Vote Value
+     */
+    public void addVote(RemoteParticipant client, String vote) {
+        // Add or overwrite current vote for the client, if they can vote
+        if(client.canVote()) {
+            this.votes.put(client.getUsername(), vote);
+        }
+        else {
+            client.pushOutput("You cannot vote on this.");
+        }
+    }
 }
